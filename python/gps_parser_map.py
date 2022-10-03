3740c1b0ec8f4fb395cbe9a8b0f4b2c65317c929
enheragu <enrique.he.ag@gmail.com>
2022-10-03 11:17:58 +0200

    Add timestamp and new graph

--- python/gps_parser_map.py
+++ python/gps_parser_map.py
@@ -38,16 +38,17 @@ def parseROSTopicLog(filename_in):
     print ("[INFO] [parseROSTopicLog] - Parsing file " + str(filename_in))
 
     """ The following regex pattern searchs in rostopic fix dumped file for the following data
-        · match[0] = status
-        · match[1] = latitude
-        · match[2] = longitude
-        · match[3] = altitude
-        · match[4] = position_covariance[0,0] # diagonal of the matrix is parsed
-        · match[5] = position_covariance[1,1]
-        · match[6] = position_covariance[2,2]
-        · match[7] = position_covariance_type
+        · match[0] = secs(timestamp)
+        · match[1] = status
+        · match[2] = latitude
+        · match[3] = longitude
+        · match[4] = altitude
+        · match[5] = position_covariance[0,0] # diagonal of the matrix is parsed
+        · match[6] = position_covariance[1,1]
+        · match[7] = position_covariance[2,2]
+        · match[8] = position_covariance_type
     """
-    pattern_reg_str = "  status: ([0-9-]+)\n  service: [0-9-.]+\nlatitude: ([0-9-.]+)\nlongitude: ([0-9-.]+)\naltitude: ([0-9-.]+)\nposition_covariance: \[([0-9-.]+), [0-9-.]+, [0-9-.]+, [0-9-.]+, ([0-9-.]+), [0-9-.]+, [0-9-.]+, [0-9-.]+, ([0-9-.]+)\]\nposition_covariance_type: ([0-9-])"
+    pattern_reg_str = '    secs: ([0-9]+)\n    nsecs: [0-9]+\n  frame_id: "gps"\nstatus: \n  status: ([0-9-]+)\n  service: [0-9-.]+\nlatitude: ([0-9-.]+)\nlongitude: ([0-9-.]+)\naltitude: ([0-9-.]+)\nposition_covariance: \[([0-9-.]+), [0-9-.]+, [0-9-.]+, [0-9-.]+, ([0-9-.]+), [0-9-.]+, [0-9-.]+, [0-9-.]+, ([0-9-.]+)\]\nposition_covariance_type: ([0-9-])'
     
 
     data = []
@@ -56,10 +57,11 @@ def parseROSTopicLog(filename_in):
         match_list = re.findall(pattern_reg_str,file)
         for index, match in enumerate(match_list):
             curr_data = {}
-            curr_data["status"] = float(match[0])
-            curr_data["position"] = [float(match[1]), float(match[2]), float(match[3])]
-            curr_data["position_covariance"] = [float(match[4]), float(match[5]), float(match[6])]
-            curr_data["position_covariance_type"] = float(match[7])
+            curr_data["secs"] = float(match[0])
+            curr_data["status"] = float(match[1])
+            curr_data["position"] = [float(match[2]), float(match[3]), float(match[4])]
+            curr_data["position_covariance"] = [float(match[5]), float(match[6]), float(match[7])]
+            curr_data["position_covariance_type"] = float(match[8])
 
             data.append(curr_data)
     # print("[INFO] [parseROSTopicLog] - Data parsed is: " + str(data))
@@ -100,8 +102,10 @@ def projectOverMapImage(filename_in, data_in):
         :param filename_in: - Path of the image to process.
         :param data_in: - Data array of 2d array with gps coordinates to project over the image
 
-        :return: Returns nothing
+        :return: time_stamp_in_image
     """
+    # List to be able to get which timestamped coordinates are inside this current image
+    time_stamp_in_image = []
 
     # Parse configuration of points in image
     data = parseYAMLConfig(filename_in.replace("jpg", "yml"))
@@ -189,6 +193,7 @@ def projectOverMapImage(filename_in, data_in):
            current_pixel_coord[0] < width and current_pixel_coord[1] < height:
             pixel_coord.append(current_pixel_coord)
             gps_status.append(current["status"])
+            time_stamp_in_image.append(current["secs"])
             error_ellipse.append([m_to_pixel_transform*sqrt(current["position_covariance"][0])*4,
                                   m_to_pixel_transform*sqrt(current["position_covariance"][1])*4])
         else:
@@ -210,9 +215,10 @@ def projectOverMapImage(filename_in, data_in):
     x_coord = [i[0] for i in pixel_coord]
     y_coord = [i[1] for i in pixel_coord]
     
-    color_dict = {-1: 'r', 0: 'r', 1: 'y', 2: 'g'}
+    #color_dict = {-1: '#ef9007', 0: '#ef9007', 1: '#f6d317', 2: '#c2e812'}
+    color_dict = {-1: 'r', 0: 'r', 1: 'y', 2: 'b'}
     color_list = [color_dict[current] for current in gps_status]
-    ax.scatter(x = x_coord, y = y_coord, marker = '+', c = color_list, s = 1)
+    ax.scatter(x = x_coord, y = y_coord, marker = '+', c = color_list, s = 10)
 
     # Computes error ellipse based on 4 times sigma
     width_ellipse = [i[0] for i in error_ellipse]
@@ -223,14 +229,106 @@ def projectOverMapImage(filename_in, data_in):
     ax.add_collection(ec)
 
     # Plot reference points used for interpolation in blue
-    ax.scatter(data[0]["pixel"][0], data[0]["pixel"][1], c='b', s=10, marker = '+')
-    ax.scatter(data[1]["pixel"][0], data[1]["pixel"][1], c='b', s=10, marker = '+')
+    ax.scatter(data[0]["pixel"][0], data[0]["pixel"][1], c='g', s=10, marker = '+')
+    ax.scatter(data[1]["pixel"][0], data[1]["pixel"][1], c='g', s=10, marker = '+')
 
     fig.savefig(filename_in.replace(".jpg","_out.jpg"), dpi=dpi, transparent=True)
 
 
-    return None
+    return time_stamp_in_image
 
+def plotXYSigmaGraph(filename_in, data_in, timestamp_in_image):
+    """
+        Plots data in a graph easyer to check.
+        :param filename_in: - Path of the image to process.
+        :param data_in: - Data array of 2d array with gps coordinates to project over the image
+        :param timestamp_in_image: - Timestamps of data that actualli is in the current image
+
+        :return: Returns nothing
+    """
+
+    import utm
+
+    # Create a figure of the right size with one axes that takes up the full figure
+    fig = plt.figure(figsize=(10,13))
+
+    # set the spacing between subplots
+    plt.subplots_adjust(left=0.11,
+                    bottom=0.06, 
+                    right=0.95, 
+                    top=0.95, 
+                    wspace=0.3, 
+                    hspace=0.2)
+
+    # Separates X and Y coordinates into different arrays and assigns a color to it based
+    # on the status in which this point was obtained
+
+    data = [utm.from_latlon(current["position"][0],current["position"][1]) for current in data_in]
+    # print(data_in[0])
+    # print(data[0])
+
+    east = data[0][0]
+    north = data[0][1]
+
+    x_coord = [current[0]-east for current in data]
+    y_coord = [current[1]-north for current in data]
+
+    # Relative time in seconds since init of test
+    time_coord = [current["secs"] for current in data_in]
+    
+    #color_dict = {-1: '#ef9007', 0: '#ef9007', 1: '#f6d317', 2: '#c2e812'}
+    color_dict = {-1: 'r', 0: 'r', 1: 'y', 2: 'b'}
+    color_list = [color_dict[current["status"]] for current in data_in]
+
+    sigma_x = [sqrt(current["position_covariance"][0]) for current in data_in]
+    sigma_y = [sqrt(current["position_covariance"][1]) for current in data_in]
+    
+    ax1 = fig.add_subplot(4,1,1)
+    # ax1.scatter(range(len(x_coord)), x_coord, c = color_list, s = 0.5) 
+    # ax1.plot(x_coord, c = "#ef9007")
+    ax1.set_ylabel("este (m)")
+    ax1.grid()
+
+    ax2 = fig.add_subplot(4,1,2, sharex = ax1)
+    # ax2.scatter(range(len(sigma_x)), sigma_x, c = color_list, s = 0.5) 
+    # ax2.plot(sigma_x, c = "#f6d317")
+    ax2.set_ylabel("σ_x (m)")
+    ax2.grid()
+
+    ax3 = fig.add_subplot(4,1,3, sharex = ax1)
+    # ax3.scatter(range(len(y_coord)), y_coord, c = color_list, s = 0.5) 
+    # ax3.plot(y_coord, c = "#1e3f54")
+    ax3.set_ylabel("norte (m)")
+    ax3.grid()
+
+    ax4 = fig.add_subplot(4,1,4, sharex = ax1)
+    # ax4.scatter(range(len(sigma_y)), sigma_y, c = color_list, s = 0.5) 
+    # ax4.plot(sigma_y, c = "#45bcee")
+    ax4.set_ylabel("σ_y (m)")
+    ax4.set_xlabel("Tiempo desde inicio (s)")
+    ax4.grid()
+
+    fig.suptitle('Desplazamiento desde 30S ' + str(round(east,3)) + ' ' + str(round(north,3)), fontsize=16)
+    for current in range(len(data)-1):
+        # If current data is not in the image just skip it
+        if time_coord[current] in timestamp_in_image and time_coord[current+1] in timestamp_in_image:
+            # More than two seconds without data, do not keep continuous line
+            if time_coord[current+1] - time_coord[current] > 2:
+                continue
+
+            # set relative timestamp
+            time_now = time_coord[current] - time_coord[0]
+            time_next = time_coord[current+1] - time_coord[0]
+            ax1.plot([time_now, time_next], [x_coord[current],x_coord[current+1]], c = color_list[current+1]) 
+            ax2.plot([time_now, time_next], [sigma_x[current],sigma_x[current+1]], c = color_list[current+1]) 
+            ax3.plot([time_now, time_next], [y_coord[current],y_coord[current+1]], c = color_list[current+1]) 
+            ax4.plot([time_now, time_next], [sigma_y[current],sigma_y[current+1]], c = color_list[current+1]) 
+
+    plt.setp(ax1.get_xticklabels(), visible=False)
+    plt.setp(ax2.get_xticklabels(), visible=False)
+    plt.setp(ax3.get_xticklabels(), visible=False)
+
+    fig.savefig(filename_in.replace(".jpg","_out_graph.jpg"), transparent=True)
 
 def projectLogsOnMap():
     from data_set_list import data_set_path, map_data_set_list
@@ -241,7 +339,8 @@ def projectLogsOnMap():
             data += parseROSTopicLog(data_set_path + data_path)
         
         for map_path in item["map"]:
-            projectOverMapImage(data_set_path +  map_path, data)
+            timestamp_points_in_image = projectOverMapImage(data_set_path +  map_path, data)
+            plotXYSigmaGraph(data_set_path +  map_path, data, timestamp_points_in_image)
 
 if __name__ == "__main__":
     # path = './2022_06_16/'
