9d106364da07766ea1bc52aae7b42f2ac934f2dd
enheragu <enrique.he.ag@gmail.com>
2022-12-02 12:39:59 +0100

    Update with zero rate gyors command sent to IMU. Add coments to setup scripts and update urdf

--- scripts/husky_setup.sh
+++ scripts/husky_setup.sh
@@ -45,16 +45,19 @@ function husky_launch_base() {
 	sudo systemctl restart ros
 }
 
+## Launch cartographer SLAM configured for the robot
 function husky_launch_slam() {
 	_husky_check_setup
 	roslaunch husky_manager cartographer_husky.launch
 }
 
+## Launch MoveBase module to worka along Cartographer SLAM and the rest of the robot modules
 function husky_launch_nav() {
 	_husky_check_setup
 	roslaunch husky_manager nav.launch
 }
 
+## Launch rviz session with some usefull topics preconfigured to be shown
 function husky_launch_rviz() {
 	_husky_check_setup
 	rosrun rviz rviz -d $HUSKY_SETUP_SCRIPT_PATH/../rviz/rviz_map_cfg.rviz
@@ -71,6 +74,23 @@ function husky_record_rosbag() {
 	rosbag record -a
 }
 
+## Finish cartographer trajectory so a serialization can be performed, then stores the pbstream in a given location
+function husky_serialize_cartographer_output()
+{
+	# Finish the first trajectory. No further data will be accepted on it.
+	rosservice call /finish_trajectory 0
+
+	# Ask Cartographer to serialize its current state.
+	# (press tab to quickly expand the parameter syntax)
+	FOLDER_DATE=$(date +%G_%m_%d_%H_%M)
+	MAP_PATH=~/map_log/$FOLDER_DATE
+	mkdir -p $MAP_PATH
+	rosservice call /write_state "{filename: '${MAP_PATH}/map.pbstream', include_unfinished_submaps: "true"}"
+	( cd ${MAP_PATH} && cartographer_pbstream_to_ros_map -pbstream_filename  ${MAP_PATH}/map.pbstream )
+	echo -e "${GREEN}Stored serialized data in: ${MAP_PATH}/map.pbstream${NC}"
+}
+
+## Command to compile husky workspace from any path
 function husky_make() {
 	_husky_check_setup
 	(cd ~/husky_noetic_ws/ &&  catkin_make --cmake-args -DCMAKE_BUILD_TYPE=Release)
@@ -86,6 +106,7 @@ function husky_launch_help() {
 	
 Other commands that might help are:
 	- ${GREEN}husky_make${NC} -> allows to make the husky customization workspace from any path.
-	- ${GREEN}husky_record_rosbag${NC} -> records a complete rosbag in ~/test_log with a folder name based on the timetag of the execution time
-	 "
+	- ${GREEN}husky_record_rosbag${NC} -> records a complete rosbag in ~/test_log with a folder name based on the timetag of the execution time.
+	- ${GREEN}husky_serialize_cartographer_output${NC} -> Serializes the output of Cartographer finishing its current trajectory and stores pbstream file with its current state.
+	"
 }
